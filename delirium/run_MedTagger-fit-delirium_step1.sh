a5701d4d93382be9518411921b13a1789848d14a
Fu <sunyangfu@gmail.com>
2026-02-17 10:48:42 -0600

    added enact-delirium model

--- delirium/run_MedTagger-fit-delirium_step1.sh
+++ delirium/run_MedTagger-fit-delirium_step1.sh
@@ -3,7 +3,7 @@
 
 INPUT_DIR="/delirium/data/input"
 OUTPUT_DIR="/delirium/data/output"
-RULES_DIR="/delirium/models/DELIRIUM_OMC_MCHS_LOCAL"
+RULES_DIR="/delirium/models/ENACT_DELIRIUM"
 
 java -Xms512M -Xmx2000M -jar MedTagger-fit-context.jar $INPUT_DIR $OUTPUT_DIR $RULES_DIR
 
